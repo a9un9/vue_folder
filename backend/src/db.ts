b7ef69310d2e8857d2aa7621a51dae93384bbdaf
Agung Hutri <agung@qtasmin.com>
2025-08-30 01:26:14 +0700

    tes

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -3,7 +3,7 @@ import { Pool } from "pg";
 export const pool = new Pool({
     user: "postgres",
     host: "localhost",
-    database: "test_db",
-    password: "bangsat",
+    database: "test_db", // Ganti dengan nama database Anda
+    password: "bangsat", // Ganti dengan password database Anda
     port: 5432,
 });
